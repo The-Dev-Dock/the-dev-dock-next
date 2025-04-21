0213cbd644538d18fea0a51d480ed4a9d8a32863
Nitant <nitant.chhajed@outlook.com>
2025-04-21 16:00:49 +0530

    config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export", 
   reactStrictMode: true,
   env: {
     EMAIL_USER: process.env.EMAIL_USER,
