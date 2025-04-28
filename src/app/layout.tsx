5a424f68de5411cc952758080f3a019605f775f6
Nitant <nitant.chhajed@systango.com>
2025-04-28 17:25:55 +0530

    new icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
     <html lang="en" className={poppins.className}>
       <head>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
+        <link rel="icon" href="favicon.ico" />
       </head>
       <body>
         {children}
